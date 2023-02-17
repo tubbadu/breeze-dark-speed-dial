2a5a11ba0794c1371faeb66791bc6ca5147fc372
tubbadu <tubbadu@gmail.com>
2023-02-17 17:31:49 +0100

    edited to remove innerHTML: now the bookmarks are saved as JSON list (stringifyied)

--- main.js
+++ main.js
@@ -1,39 +1,58 @@
-let bookmarks = localStorage.getItem("bookmarks");
 let configureTarget = null;
-
-if(bookmarks !== null && bookmarks.length > 20){
-	console.log("valid saving")
-	document.getElementById("buttons-container").innerHTML = bookmarks;
-} else {
-	console.log("invalid saving")
+let bookmarks;
+try {
+	bookmarks = JSON.parse(localStorage.getItem("bookmarksJSON"))
+	if(bookmarks === null || bookmarks === undefined){
+		throw new Error('empty saving');
+	}
+} catch (e) {
+	// default values
+	bookmarks = [
+		{
+			title: "KDE",
+			src: "https://kde.org/",
+			img: "https://kde.org/stuff/clipart/logo/kde-logo-white-blue-rounded-source.svg"
+		},
+		{
+			title: "Linux.org",
+			src: "https://linux.org/",
+			img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Tux.png/220px-Tux.png"
+		},
+		{
+			title: "Mozilla",
+			src: "https://mozilla.org/",
+			img: "https://www.mozilla.org/media/protocol/img/logos/mozilla/logo-word-hor.e20791bb4dd4.svg"
+		}
+	]
 }
 
 
+for (let bookmark of bookmarks) {
+	addBookmark(bookmark.title, bookmark.src, bookmark.img);
+}
 
 function updateBookmark(name, url, imageUrl, target){ // update an existing bookmark
 	console.log("updating bookmark")
 	target.querySelector(".IMG").src = imageUrl
 	target.href = url
-	target.querySelector(".TITLE").innerHTML = name
+	target.querySelector(".TITLE").textContent = name
 }
 
 function addBookmark(name, url, imageUrl){ // create a new bookmark
 	document.querySelector("#button-template .IMG").src = imageUrl
 	document.querySelector("#button-template").href = url
-	document.querySelector("#button-template .TITLE").innerHTML = name
+	document.querySelector("#button-template .TITLE").textContent = name
 
 	var node = document.getElementById("button-template").cloneNode(true);
 	node.id = "";
 	
 	document.getElementById("buttons-container").appendChild(node)
-	//modal.style.display = "none";
-	//localStorage.setItem("bookmarks", parent.innerHTML)
 }
 
 function configureBookmark(event){ // open the popup to edit an existing bookmark
 	event.preventDefault();
 	let currentButton = document.querySelector(".breeze-button:hover");
-	let name = currentButton.querySelector(".TITLE").innerHTML
+	let name = currentButton.querySelector(".TITLE").textContent
 	let url = currentButton.href
 	let imageUrl = currentButton.querySelector(".IMG").src
 	configureTarget = currentButton;
@@ -44,7 +63,7 @@ function showPopup(popupTitle="Add new bookmark", name="", url="", imageUrl=""){
 	configureImage.value = imageUrl
 	configureUrl.value = url
 	configureName.value = name
-	configureTitle.innerHTML = popupTitle
+	configureTitle.textContent = popupTitle
 	popup.style.display = "block";
 }
 
@@ -56,12 +75,15 @@ function hidePopup(){
 
 function saveConfig(){
 	console.log("saving")
-	localStorage.setItem("bookmarks", document.getElementById("buttons-container").innerHTML)
+	localStorage.setItem("bookmarks", document.getElementById("buttons-container").textContent)
+	let data = getButtonData();
+	localStorage.setItem("bookmarksJSON", JSON.stringify(data))	
 }
 
 function deleteStorage(){ // this will remove all saved bookmarks. Use with caution
 	console.log("deleting storaged bookmarks")
 	localStorage.setItem("bookmarks", null)
+	localStorage.setItem("bookmarksJSON", null)
 }
 
 function search(query){ // searches for the given query using the default search engine
@@ -71,6 +93,19 @@ function search(query){ // searches for the given query using the default search
 		let currentTabId = currentTab.id;
 		browser.search.search({query: query, tabId: currentTabId})
 	});
+}
+
+function getButtonData() {
+	const buttonData = [];
+	const buttonsContainer = document.getElementById("buttons-container");
+	const buttons = buttonsContainer.querySelectorAll("a:not(#button-template)");
+	buttons.forEach((button) => {
+		const img = button.querySelector(".IMG").getAttribute("src");
+		const title = button.querySelector(".TITLE").textContent;
+		buttonData.push({ title, img });
+	});
+
+	return buttonData;
 }
 
 document.addEventListener('click', function(event) {
@@ -85,7 +120,7 @@ window.addEventListener('load', function() { // not working as expected
 });
 
 document.addEventListener('DOMContentLoaded', function() { // not working
-    document.querySelector('#search-form input').focus();
+	document.querySelector('#search-form input').focus();
 });
 
 addNew.onclick = function() {showPopup()} // show popup when the + button is clicked
@@ -116,8 +151,6 @@ newBookmark.onsubmit = function(event){ // handle the save button click, or the 
 	}
 	
 	hidePopup();
-
-	//localStorage.setItem("bookmarks", parent.innerHTML)
 }
 
 configureTrash.onclick = function(event){ // handle the trash button click
@@ -153,7 +186,7 @@ let currentDraggedItem = null;
 for (const item of items) {
   item.addEventListener("dragstart", function(e) {
     currentDraggedItem = this;
-    e.dataTransfer.setData("text/plain", this.innerHTML);
+    e.dataTransfer.setData("text/plain", this.textContent);
   });
 
   item.addEventListener("dragover", function(e) {
